27d47108b0e7b6a777159d61a496e496380fbb2d
M. Ochoa <40745550+mochoabuilds@users.noreply.github.com>
2020-06-13 23:33:42 -0500

    Update centroidtracker.py

--- centroidtracker.py
+++ centroidtracker.py
@@ -29,4 +29,38 @@ def deregister(self, objectID):
 	del self.objects[objectID]
 	del self.disappeared[ObjectId]
 
-# heart of centroid tracker
+# accept bounding box rectangles from object detector
+def update(self, rects):
+	# check if bounding box is empty
+	if len(rect) == 0:
+		# if empty, loop over ObjectIDs and mark as disappeared
+		for objectID in list(self.disappeared.keys()):
+			self.disappeared[objectID] += 1
+			
+			# remove given object from tracking system if marked missing
+			if self.disappeared[objectID] > self.maxDisappeared:
+				self.deregister(objectID)
+	
+		# return early if no centroid or tracking info to update
+		return self.objects
+	
+	# initialize a NumPy array to store centroids for each rect
+	inputCentroids = np.zeros((len(rects), 2), dtype="int")
+	
+# loop over bounding box rectangles 
+for (i, (startX, startY, endX, endY)) in enumerate(rects):
+	# compute the centroid from bounding box coordinates and store it 
+	cX = int((startX + endX) / 2.0)
+	cY = int((startY + endY) / 2.0)
+	inputCentroids[i] = (cX, cY)
+	
+# if not currently tracking objects, then register input centroids of the new objects
+if len(self.objects) == 0:
+	for i in range(0, len(inputCentroids)):
+		self.register(inputCentroids[i])
+
+# otherwise,
+	
+
+	
+
